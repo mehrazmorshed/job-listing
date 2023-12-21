2001f29cb788fbe4577401aece0214d09cb572a1
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-12-21 14:48:34 +0600

    declare toString function

--- lib/Template.php
+++ lib/Template.php
@@ -16,4 +16,14 @@
 	public function __set($key, $value){
 		$this->vars[$key] = $value;
 	}
+
+	public function __toString(){
+		extract($this->vars);
+		chdir(dirname($this->template));
+		ob_start();
+
+		include basename($this->template);
+
+		return ob_get_clean();
+	}
 }
