17348be08dfa6254429688860ddbe32a20b90836
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-12-26 22:06:47 +0600

    Update header.php

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Job Listing</title>
+	<link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
