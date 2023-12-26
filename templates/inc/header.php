f340c1129cf4e71c6c5c2d6cd7dc7d0b1c572fb5
Mehraz Morshed <mehrazmorshed@gmail.com>
2023-12-26 22:15:49 +0600

    Update header.php

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -6,3 +6,14 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<div class="container">
+		<div class="header clearfix">
+			<nav>
+			<ul class="nav nav-pills pull-right">
+				<li role="presentation" class="active"><a href="#">Home</a></li>
+				<li role="presentation"><a href="#">About</a></li>
+				<li role="presentation"><a href="#">Contact</a></li>
+			</ul>
+			</nav>
+			<h3 class="text-muted">Project Name</h3>
+		</div>
